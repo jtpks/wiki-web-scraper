674f11c7a6f4f699f5c927f3566227d84b7758e3
jahnavitp7 <jahnavi7@illinois.edu>
2025-05-17 13:28:41 -0700

    Web Scrapping Wiki Page

---
+++ src/main/java/com/example/MyScrapper.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class MyScrapper {
+    public static void main(String[] args)
+    {
+        try {
+            Document doc
+                = Jsoup
+                      .connect("https://en.wikipedia.org/wiki/Main_Page")
+                      .get();
+
+            Elements links = doc.select("a[href]");
+            Elements images = doc.select("img[src]");
+
+            System.out.println("Links: ");
+            for (Element link : links) {
+                System.out.println(link.attr("href"));
+            }
+
+            System.out.println("\n-----\n");
+
+            System.out.println("Images:");
+            for (Element image : images) {
+                System.out.println(image.attr("src"));
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
